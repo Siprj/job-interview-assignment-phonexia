42b05fbb43fafa6a605458785e404444453c4c5f
Jan Sipr <sipr.jan@gmail.com>
2017-08-24 21:27:08 +0200

    Collect statistics

--- main.cpp
+++ main.cpp
@@ -14,20 +14,23 @@ using namespace io;
 #define INTERNAL_CALL_STR "VS"
 #define INTERNAL_SMS_STR "SS"
 
-#define FEE_EXTERNAL_CALL 3.5
+#define FEE_EXTERNAL_CALL (3.5 / 60.0)
 #define FEE_EXTERNAL_SMS 2.0
-#define FEE_INTERNAL_CALL 1.5
+#define FEE_INTERNAL_CALL (1.5 / 60.0)
 #define FEE_INTERNAL_SMS 1.0
 #define FREE_MINUTES 100
 #define FREE_NUMBERS "+420732563345", "+420707325673"
-#define FREE_SMS 10
+#define FREE_SMSS 10
 #define MONTHLY_FEE 900
 
-enum EntryType
-    { InternalCall
-    , ExternalCall
-    , InternalSMS
-    , ExternalSMS
+enum ChannelType
+    { Call
+    , SMS
+    };
+
+enum DestinationType
+    { Internal
+    , External
     };
 
 class Entry
@@ -35,7 +38,8 @@ class Entry
 public:
     string number;
     int duration;
-    EntryType type;
+    ChannelType channelType;
+    DestinationType destinationType;
 };
 
 class Free
@@ -60,6 +64,7 @@ public:
         internalSMSCost = 0;
         externalSMSAmount = 0;
         externalSMSCost = 0;
+        calls = 0;
     }
 
     float totalCost;
@@ -71,6 +76,7 @@ public:
     float internalSMSCost;
     int externalSMSAmount;
     float externalSMSCost;
+    int calls;
 };
 
 vector<Entry> retrieveData(const char* filePath)
@@ -110,19 +116,23 @@ vector<Entry> retrieveData(const char* filePath)
 
         if (stringifiedType.compare(INTERNAL_CALL_STR))
         {
-            entry.type = InternalCall;
+            entry.channelType = Call;
+            entry.destinationType = Internal;
         }
         else if (stringifiedType.compare(EXTERNAL_CALL_STR))
         {
-            entry.type = ExternalCall;
+            entry.channelType = Call;
+            entry.destinationType = External;
         }
         else if (stringifiedType.compare(INTERNAL_SMS_STR))
         {
-            entry.type = InternalSMS;
+            entry.channelType = SMS;
+            entry.destinationType = Internal;
         }
         else if (stringifiedType.compare(EXTERNAL_SMS_STR))
         {
-            entry.type = ExternalCall;
+            entry.channelType = SMS;
+            entry.destinationType = External;
         }
         else
         {
@@ -137,11 +147,20 @@ vector<Entry> retrieveData(const char* filePath)
     return data;
 }
 
+int rampSeconds(int secs)
+{
+    if (secs < 60)
+    {
+        return 60;
+    }
+    return secs;
+}
+
 CallStatistics collectStatistics(vector<Entry> data)
 {
-    CallStatistics stats;
-    float freeMinutes = FREE_MINUTES;
-    float freeSMS = FREE_SMS;
+    CallStatistics statistics;
+    int freeCallSeconds = FREE_MINUTES * 60;
+    int freeSMSs = FREE_SMSS;
 
     for (auto item: data)
     {
@@ -150,18 +169,145 @@ CallStatistics collectStatistics(vector<Entry> data)
         {
             // Numbers free of charge
             cout << "Number: " << item.number << " duration: " << item.duration << endl;
+            switch (item.channelType)
+            {
+            case Call:
+                statistics.calls++;
+
+                switch (item.destinationType)
+                {
+                case Internal:
+                    statistics.internalCallTime += item.duration;
+                    break;
+                case External:
+                    statistics.externalCallTime += item.duration;
+                    break;
+                }
+                break;
+            case SMS:
+                switch (item.destinationType)
+                {
+                case Internal:
+                    statistics.internalSMSAmount++;
+                    break;
+                case External:
+                    statistics.externalSMSAmount++;
+                    break;
+                }
+                break;
+            }
         }
         else
         {
+            switch (item.channelType)
+            {
+            case Call:
+                statistics.calls++;
+
+                if (freeCallSeconds > 0)
+                {
+                    freeCallSeconds -= item.duration;
+                    int residualSeconds = 0 - freeCallSeconds;
+                    if (residualSeconds > 0)
+                    {
+                        switch (item.destinationType)
+                        {
+                        case Internal:
+                            statistics.internalCallCost +=
+                                rampSeconds(residualSeconds) * FEE_INTERNAL_CALL;
+                            break;
+                        case External:
+                            statistics.externalCallCost +=
+                                rampSeconds(residualSeconds) * FEE_EXTERNAL_CALL;
+                            break;
+                        }
+                    }
+
+                    switch (item.destinationType)
+                    {
+                    case Internal:
+                        statistics.internalCallTime += item.duration;
+                        break;
+                    case External:
+                        statistics.externalCallTime += item.duration;
+                        break;
+                    }
+                }
+                else
+                {
+                    switch (item.destinationType)
+                    {
+                    case Internal:
+                        statistics.internalCallTime += item.duration;
+                        statistics.internalCallCost +=
+                            rampSeconds(item.duration) * FEE_INTERNAL_CALL;
+                        break;
+                    case External:
+                        statistics.externalCallTime += item.duration;
+                        statistics.externalCallCost +=
+                            rampSeconds(item.duration) * FEE_EXTERNAL_CALL;
+                        break;
+                    }
+                }
+                break;
+            case SMS:
+                if (freeSMSs > 0)
+                {
+                    freeSMSs--;
+
+                    switch (item.destinationType)
+                    {
+                    case Internal:
+                        statistics.internalSMSAmount++;
+                        break;
+                    case External:
+                        statistics.externalSMSAmount++;
+                        break;
+                    }
+                }
+                else
+                {
+                    switch (item.destinationType)
+                    {
+                    case Internal:
+                        statistics.internalSMSAmount++;
+                        statistics.internalSMSCost += FEE_INTERNAL_SMS;
+                        break;
+                    case External:
+                        statistics.externalSMSAmount++;
+                        statistics.externalSMSCost += FEE_EXTERNAL_SMS;
+                        break;
+                    }
+                }
+                break;
+            }
         }
     }
-    return stats;
+
+    statistics.totalCost = MONTHLY_FEE
+        + statistics.internalCallCost
+        + statistics.externalCallCost
+        + statistics.internalSMSCost
+        + statistics.externalSMSCost;
+
+    return statistics;
 }
 
 int main()
 {
     auto data = retrieveData("data/data.csv");
     auto statistics = collectStatistics(data);
+
+    cout << "Monthly fee: " << MONTHLY_FEE << endl;
+    cout << "Number of calls: " << statistics.calls << endl;
+    cout << "Number of SMSs: " << statistics.internalSMSAmount
+        + statistics.externalSMSAmount << endl;
+    cout << "Total cost: " << statistics.totalCost << endl;
+
+    cout << "Call cost: " << statistics.internalCallCost
+        + statistics.externalCallCost << endl;
+    cout << "SMS cost: " << statistics.internalSMSCost
+        + statistics.externalSMSCost << endl;
 
     return 0;
 }
