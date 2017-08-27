654e6013731e3aa7415e9097cd776075759e1b83
Jan Sipr <sipr.jan@gmail.com>
2017-08-27 14:18:33 +0200

    Straight forward command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,21 @@
 #include "parser.h"
 #include "statistics.h"
 #include "constants.h"
+#include "cmdline.h"
 
 
 using namespace std;
+using namespace cmdline;
 
-int main()
+int main(int argc, char *argv[])
 {
-    auto data = retrieveData("data/data.csv");
+    parser cmd;
+    cmd.add<string>("file", 'f', "Path to data file", true, "");
+
+    cmd.parse_check(argc, argv);
+
+
+    auto data = retrieveData(cmd.get<string>("file").c_str());
     auto statistics = collectStatistics(data);
 
     cout << pretty(statistics);
